723c6fbae6ce1123762aaeb0a16e5c0ffa49c6e0
Jeff Frazier <jeff@blackbear.io>
2022-03-19 16:48:34 -0400

    Begin custom plugin

---
+++ wp-content/plugins/woocommerce-custom-product-fields/woocommerce-custom-product-fields.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package woocommerce-custom-product-fields
+ */
+/*
+Plugin Name: woocommerce-custom-product-fields
+Plugin URI: http://blackbear.io
+Description: Blackbear.io plugin for WooCommerce custom product fields
+Version: 1.0.0
+Author: Blackbear Development
+Author URI: http://blackbear.io
+License: GPLv2 or later
+Text Domain: woocommerce-custom-product-fields
+*/
